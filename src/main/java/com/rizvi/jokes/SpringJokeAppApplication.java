d0d00230db14cd08352764eb848cee617c750efd
syedincharge <rizvisyedba@gmail.com>
2022-03-13 18:45:13 -0600

    Initial commit

---
+++ src/main/java/com/rizvi/jokes/SpringJokeAppApplication.java
@@ -0,0 +1,13 @@
+package com.rizvi.jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJokeAppApplication.class, args);
+	}
+
+}
